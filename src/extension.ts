401c702ebf92763148eaf558b59bd5900023091b
Wendy Chan <wenwkich@gmail.com>
2022-11-22 10:20:46 -0500

    feat: show basic graphviz and wire up vue.js

--- src/extension.ts
+++ src/extension.ts
@@ -17,10 +17,15 @@ export function activate(context: vscode.ExtensionContext) {
         "callGraph", // Identifies the type of the webview. Used internally
         "Call Graph", // Title of the panel displayed to the user
         vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
-        {} // Webview options. More on these later.
+        {
+          enableScripts: true,
+        }
       );
 
-      panel.webview.html = getWebviewContent();
+      panel.webview.html = getGraphContent("main", {
+        main: "digraph { a -> b }",
+      });
+      console.log(panel.webview.html);
     }
   );
 
@@ -30,16 +35,44 @@ export function activate(context: vscode.ExtensionContext) {
 // This method is called when your extension is deactivated
 export function deactivate() {}
 
-function getWebviewContent() {
-  return `<!DOCTYPE html>
+export function getGraphContent(graphId: string, dots: Record<string, string>) {
+  return `
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cat Coding</title>
-</head>
-<body>
-    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
-</body>
-</html>`;
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="Content-Security-Policy" 
+      content="default-src 'none'; worker-src blob:; connect-src https://unpkg.com; script-src https://d3js.org https://unpkg.com https://cdn.jsdelivr.net 'unsafe-eval' 'unsafe-inline' 'wasm-unsafe-eval';">
+    <title>Call Graph</title>
+  </head>
+  <body>
+    <div id="app">
+      {{graphId}}
+      <div id="graph"></div>
+    </div>
+
+    <script src="https://d3js.org/d3.v5.min.js"></script>
+    <script src="https://unpkg.com/@hpcc-js/wasm@0.3.11/dist/index.min.js"></script>
+    <script src="https://unpkg.com/d3-graphviz@3.0.5/build/d3-graphviz.js"></script>
+    <script
+      src="https://unpkg.com/@hpcc-js/wasm@2.1.1/dist/index.js"
+      type="application/javascript"
+    ></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.7.13"></script>
+    <script>
+      let app = new Vue({
+        el: "#app",
+        data: {
+          dots: ${JSON.stringify(dots)},
+          graphId: "${graphId}",
+        },
+        mounted() {
+          d3.select("#graph").graphviz().renderDot(this.dots[this.graphId]);
+        },
+      });
+    </script>
+  </body>
+</html>
+  `;
 }
